ffa360104bdbd3427237aa0177b4e7f301aefeba
3le4ms1 <ismaeledegiorgi@gmail.com>
2024-04-15 17:23:57 +0200

    [emacs] fix `_foreign.el' e aggiunta commenti

--- emacs/foreign/_foreign.el
+++ emacs/foreign/_foreign.el
@@ -1,7 +1,13 @@
 ;;; _foreign.el --- Configurazione dei file in repos esterni
 
 ;;; Commentary:
-;;; Configurazione dei vari repository con modes varie
+;;; Configurazione dei vari repository con packages vari.
+;;; I vari packages esterni sono contenuti nella variabile `foreign-packages',
+;;; secondo il seguente formato:
+;;; `(package-name package-init-file package-repo-link)'
+;;; La funzione `3le/check-dir-exists' prende tutta la lista che descrive un
+;;; package (quella sopra), e fa il controllo per vedere se è già scaricata. Se
+;;; non è presente, la scarica, e la carica
 
 ;;; Code:
 
@@ -14,11 +20,13 @@
 (defun 3le/check-dir-exists (package)
   "Automatic download and load of PACKAGE."
   (progn
+    ;; (unless cond) == (if (not (cond)))
+    ;; check per la directory del package
     (unless (file-exists-p (concat 3le/emacs-conf-dir "/foreign/" (car package)))
       (shell-command (concat "git clone --depth=1 " (car (cdr (cdr package))) " "
                              3le/emacs-conf-dir "/foreign/" (car package))))
-    (mapc #'load (file-expand-wildcards (concat 3le/emacs-conf-dir "/foreign/"
-                                                (car x) "/" (car (cdr package)))))))
+    (load (concat 3le/emacs-conf-dir "/foreign/"
+                  (car package) "/" (car (cdr package))))))
 
 (mapc #'3le/check-dir-exists foreign-packages)
 
