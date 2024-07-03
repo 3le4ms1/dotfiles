78fe4616709c21e1521af9fc7fdd9483f4bf661c
3le4ms1 <ismaeledegiorgi@gmail.com>
2024-07-03 23:02:17 +0200

    [emacs] emacs_startup script fix

--- emacs/emacs_startup.cmd
+++ emacs/emacs_startup.cmd
@@ -1,8 +1,9 @@
-@rem Script di auto start di Emacs su Windows
-@rem https://stackoverflow.com/questions/19047442/my-batch-file-keeps-looping-but-why
-
 @echo off
-where /q emacs
+rem Script di auto start di Emacs su Windows
+rem https://stackoverflow.com/questions/19047442/my-batch-file-keeps-looping-but-why
+
+where /q runemacs
 if %errorlevel% equ 0 (
-   start /B emacs --daemon
+    runemacs --daemon >nul 2>&1
 )
+exit 0
